8c5708a426c63213d73c483d93b7428bbeee2358
王静 <wangjing_916@163.com>
2023-03-09 18:46:25 +0800

    修改打包

--- packages/utils/publish/index.ts
+++ packages/utils/publish/index.ts
@@ -1,6 +1,7 @@
 import run from '../../components/script/utils/run';
+import { pkgPath } from '../../components/script/utils/paths';
 import { series } from 'gulp';
 export const publishComponent = async () => {
-    run('release-it', `.`);
+    run('release-it', `${pkgPath}/utils`);
 };
 export default series(async () => publishComponent());
