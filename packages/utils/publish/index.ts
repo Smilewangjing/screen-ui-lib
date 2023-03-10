ce7d9a6ebd7fcb3d26f2380cc5a46979fda6cdd3
王静 <wangjing_916@163.com>
2023-03-10 15:27:05 +0800

    🐞 fix: 修改发布命令

--- packages/utils/publish/index.ts
+++ packages/utils/publish/index.ts
@@ -2,6 +2,6 @@ import run from '../../components/script/utils/run';
 import { pkgPath } from '../../components/script/utils/paths';
 import { series } from 'gulp';
 export const publishComponent = async () => {
-    run('release-it', `${pkgPath}/utils`);
+    run('pnpm publish', `${pkgPath}/utils`);
 };
 export default series(async () => publishComponent());
